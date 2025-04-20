942dc0f387472b770d2708a7180537cbb8c4adca
Uziel765 <uzielagustinvidela@gmail.com>
2025-04-20 19:30:11 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# ZENDA
+
+App multi-rubro: almacenes, kioscos, mensajerías, restaurantes y más.
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100vh",
+      fontFamily: "sans-serif"
+    }}>
+      <h1>Bienvenido a ZENDA</h1>
+      <p>Tu app multi-rubro. Empecemos...</p>
+    </div>
+  )
+}
